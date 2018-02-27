5779ae7b2bf123a7457c3541b594e0e6779446ce
Marco GESSA <Marco GESSA>
2018-02-28 00:19:49 +0100

    team

--- site/src/components/authors.vue
+++ site/src/components/authors.vue
@@ -1,6 +1,27 @@
 <template lang="pug">
     div(class="container")
-        h1 L'équipe
+        h1(class="center") L'équipe
+        div(class="row")
+            div(class="col xl3 l6")
+                center
+                    img(src="../assets/arman.png" class="teamPic")
+                    div(class="name") Arman
+                    div(class="description") Membre du groupe
+            div(class="col xl3 l6")
+                center
+                    img(src="../assets/seb.png" class="teamPic")
+                    div(class="name") Sébastien
+                    div(class="description") Membre du groupe
+            div(class="col xl3 l6")
+                center
+                    img(src="../assets/mzk.png" class="teamPic")
+                    div(class="name") Marzouk
+                    div(class="description") Membre du groupe
+            div(class="col xl3 l6")
+                center
+                    img(src="../assets/marco.jpg" class="teamPic")
+                    div(class="name") Marco
+                    div(class="description") Développeur chez Arkanse, il a contribué au développement du site
 </template>
 
 <script>
@@ -12,5 +33,19 @@ export default {
 <style>
 .container {
   margin-bottom: 200px;
+}
+.teamPic {
+    border-radius: 100%;
+    width: 200px;
+    height: 200px;
+}
+.row {
+    margin-top: 50px;
+}
+.name {
+    font-size: 2em
+}
+.description {
+    font-size: 1.2em
 }
 </style>
