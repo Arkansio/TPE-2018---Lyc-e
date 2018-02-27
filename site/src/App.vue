6f521f8e76cf1a98ab86851408cdb2c39c0e8100
Marco GESSA <Marco GESSA>
2018-02-27 17:17:45 +0100

    navbar

--- site/src/App.vue
+++ site/src/App.vue
@@ -22,8 +22,4 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-
-.brand-logo {
-  margin-left: 20px;
-}
 </style>
--- site/src/components/navbar.vue
+++ site/src/components/navbar.vue
@@ -2,22 +2,38 @@
     nav
       div(class="nav-wrapper")
         a(href="#" class="brand-logo") CRYPT-TPE
+        a(href="#" data-activates="mobile-demo" class="button-collapse")
+            i(class="material-icons") menu
         ul(id="nav-mobile" class="right hide-on-med-and-down")
           li
-            a(href="sass.html") Sass
+            a(href="sass.html") AtBash
           li
-            a(href="sass.html") Sass
+            a(href="sass.html") Scytale
+          li
+            a(href="sass.html") Code césar
+          li
+            a(href="sass.html") AUTEURS
+        ul(class="side-nav" id="mobile-demo")
+            li
+                a(href="sass.html") Sass
+            li
+                a(href="badges.html") Components
+            li
+                a(href="collapsible.html") Javascript
+            li
+                a(href="mobile.html") Mobile
 
 </template>
 
 <script>
 export default {
-  name: 'navar'
+  name: 'navbar',
+  mounted: function () {
+    // eslint-disable-next-line
+    $('.button-collapse').sideNav()
+  }
 }
 </script>
 
 <style>
-.brand-logo {
-  margin-left: 20px;
-}
 </style>
